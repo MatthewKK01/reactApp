d502db1c99df87714bcd98174b0f264f66a10b4f
MatthewKK01 <mateyavelashvili@gmail.com>
2022-09-26 21:16:59 +0400

    FirstReactApp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
+import GroceryCart from './GroceryCart';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ItemList from "./itemList";
+import { produce, pantryItems } from "./storeItems";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<GroceryCart/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
